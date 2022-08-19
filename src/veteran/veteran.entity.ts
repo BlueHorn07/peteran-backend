ff3db0704b1f5d78ed9d26d38afb55903371dc77
Seokyun Ha <bluehornblues@gmail.com>
2022-08-20 02:57:04 +0900

    create veteran entity

---
+++ src/veteran/veteran.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'veteran',
+})
+export class VeteranEntity {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ nullable: true })
+  email: string;
+
+  @Column({ nullable: true })
+  phone: string;
+
+  @Column({ nullable: true })
+  field: string[]; // 전문 분야
+
+  @Column({ type: 'text', nullable: true })
+  short_description: string;
+
+  @Column({ nullable: true })
+  tag: string[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Column()
+  last_login_at: Date;
+}
